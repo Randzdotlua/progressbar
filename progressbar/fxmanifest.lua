ef28195cbb01aaaec72adf09a4edad507d7d172f
Reid <reidrandz@gmail.com>
2025-05-02 20:29:54 +0100

    Progressbar

---
+++ progressbar/fxmanifest.lua
@@ -0,0 +1,17 @@
+fx_version 'cerulean'
+lua54 'yes'
+game 'gta5'
+
+author 'randz edited qbcore progressbar'
+description 'Prodigy Inspired Progress Bar / its not like prodigy but its as close as i can get it'
+version '1.0.0'
+
+ui_page 'html/index.html'
+
+client_script 'client.lua'
+
+files {
+    'html/index.html',
+    'html/style.css',
+    'html/script.js'
+}
